9cd7dbb61d34a0f9153d0f95f4e1001f6f1d6df9
hidayatullah <22ftt1514@student.pb.edu.bn>
2024-04-01 11:44:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movieflix',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
